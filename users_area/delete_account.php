8b32482f51418e4625d5b875f5a703c2aa7f4d82
Dothuan6 <thuann6222@gmail.com>
2023-08-31 20:40:10 +0700

    De-mo

--- users_area/delete_account.php
+++ users_area/delete_account.php
@@ -1,27 +1,4 @@
-<!-- //   include_once("../includes/connect.php");
-//   include_once('../functions/common_function.php');
-//   @session_start();
-//  -->
-<!-- <h2 class="text-center mt-5 text-success">Deleting Account</h2>
-<form action="" method="post">
-    <input name="delete_account" type="submit" value="Delete account" class=" w-50 m-auto btn btn-outline-dark mt-4">
-    <input name="dont_delete_account" type="submit" value="Don't delete account" class="mb-3 w-50 m-auto btn btn-outline-dark mt-4">
-</form>
-
-    // if(isset($_POST['dont_delete_account'])){
-    //     echo "<script>window.open('profile.php','_self')</script>";
-    //     }
-    // if(isset($_POST['delete_account'])){
-    //     $username=$_SESSION['username'];
-    //     $delete_account = "Delete from `user_table` where username='$username'";
-    //     $result=mysqli_query($con,$delete_account);
-    //     if($result){
-    //         echo "<script>alert('An account have been deleted!')</script>";
-    //         echo "<script>window.open('user_login.php','_self')</script>";
-    //     }
-    // } -->
-
-    <h3 class="text-success mb-4 mt-5">Delete Account</h3>
+<h3 class="text-success mb-4 mt-5">Delete Account</h3>
     <form action="" method="post" class="mt-5">
         <div class="form-outline mb-4 w-50 m-auto">
             <input type="submit" class="form-control btn btn-outline-dark" name="delete" value="Delete account">
