f9c9ebb33511f262501f2aee2a00c1766ce465ea
Dothuan6 <thuann6222@gmail.com>
2023-09-03 23:35:59 +0700

    demo

--- admin_area/delete_payments.php
+++ admin_area/delete_payments.php
@@ -4,8 +4,8 @@ if(isset($_GET['delete_payments'])){
     $delete_payments = "delete from `user_payments` where payment_id='$payment_id'";
     $result_payments=mysqli_query($con,$delete_payments);
     if($result_payments){
-        echo "<script>alert('Successfully!')</script>";
-        echo "<script>window.open('./index.php?list_payments','_self')</script>";
+        echo "<script>alert('Xóa thành công!')</script>";
+        echo "<script>window.open('./index.php?view_payments','_self')</script>";
     }
 } 
 ?>
